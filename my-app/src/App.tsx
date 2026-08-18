a6ee1583a9b2a407bf355f0a88cd4fbdd0978d21
LuizVMaciel <luizvmaciel26@gmail.com>
2026-08-18 09:52:06 -0300

    feat: criando boilerplate

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+
+export default function app(){
+  return(
+    <div>
+      <Cabecalho/>
+      <Conteudo/>
+    </div>
+  )
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,10 @@
+export default function Conteudo(){
+    return(
+        <div>
+            <main>
+                <h2>Conteudo</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates exercitationem mollitia sit quos culpa blanditiis voluptatibus iste consequatur. Sint sunt nihil, laudantium quae totam eum commodi eius ipsa optio?</p>
+            </main>
+        </div>
+    );
+}
