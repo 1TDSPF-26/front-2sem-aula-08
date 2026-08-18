3aad8748cc661036afd8ed9a121dab40f07bc532
LuizHenriqueAAlbarello <luizhenriquealvesalbarello@gmail.com>
2026-08-18 09:54:22 -0300

    feat: Criação do boiler-plate e criação do componente inicial.

---
+++ my-app/src/App.tsx
@@ -0,0 +1,20 @@
+import Cabecalho from "./assets/components/Cabecalho/cabecalho";
+import Rodape from "./assets/components/Main/rodape";
+
+  export default function App(){
+
+    return(
+      <div>
+      <Cabecalho/>
+      </div>
+    );
+
+
+    return(
+      <div>
+      <Rodape/>
+      </div>
+    );
+  }
+
+  
---
+++ my-app/src/assets/components/Cabecalho/cabecalho.tsx
@@ -0,0 +1,22 @@
+export default function Cabecalho(){
+
+    return(
+              <div>
+        <header>
+          <h1>Meu Componente</h1>
+          <nav>
+            <ul>
+              <li>item-1</li>
+              <li>item-2</li>
+              <li>item-3</li>
+              <li>item-4</li>
+            </ul>
+          </nav>
+        </header>
+
+        
+      
+      
+      </div>
+    );
+}
