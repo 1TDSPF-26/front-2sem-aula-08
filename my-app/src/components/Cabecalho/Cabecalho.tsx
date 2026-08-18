307b737a74e2fbde06a7767f5193d28e9c5b40eb
yasminmartins18 <yasmin.s.s.martins1804>
2026-08-18 09:49:59 -0300

    feat: Confecção de rodapé cabecalho e main com

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,15 @@
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <h1>Meu componente</h1>
+            <nav>
+                <ul>
+                    <li>item 1</li>
+                    <li>item 2</li>
+                    <li>item 3</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,9 @@
+export default function Rodape(){
+
+    return(
+        <footer>
+            <h3>Meu rodapé</h3>
+            <p>&copy; Meu app lindooo</p>
+        </footer>
+    );
+}
