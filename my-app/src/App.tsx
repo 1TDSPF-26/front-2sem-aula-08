c4a5ee08cc316f4e260ec2d09ed6ea649e23363d
ogmatheus7 <yudimatheus759@gmail.com>
2026-08-18 11:36:59 -0300

    feat: cria componentes Cabecalho e Rodape

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import Cabecalho from "./components/cabecalho/Cabecalho";
+import Main from "./components/Main";
+import Rodape from "./components/Rodape";
+
+export default function App(){
+  return(
+    <div>
+    <Cabecalho/>
+    </div>
+  );
+
+  return(
+    <div>
+      <Rodape/>
+      <Main/>
+    </div>
+  )
+
+}
---
+++ my-app/src/components/Main.tsx
@@ -0,0 +1,9 @@
+export default function Main(){
+    return(
+        <div>
+            <h1>Meu Componente</h1>
+            <p>Este é o conteúdo do meu componente.</p>
+        </div>
+
+    )
+}
