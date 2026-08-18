860cd4b5b1f052d7687710fd482ee028d1a82470
Gabriel <gabrielrazod9j@gmail.com>
2026-08-18 09:50:47 -0300

    feat: Adicionando o conteudo e o cabecalho na pagina

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "./components/Cabecalho/cabecalho";
+import Conteudo from "./components/Conteudo/Conteudo";
+
+export default function app(){
+
+return(
+  <div>
+    <Cabecalho/>
+    <Conteudo/>
+  </div>
+);
+
+}
---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,12 @@
+export default function Conteudo() {
+
+    return (
+        <main>
+            <h2>Conteudo:</h2>
+                <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi, quo dolorum? Earum odio dolor vero, exercitationem maxime veniam reiciendis assumenda omnis ipsum quas distinctio hic, quasi possimus porro alias. Vero.
+                </p>
+
+        </main>
+    )
+}
