9fc2294992274e8e7bad15a989b0b0f37adb280e
gujardimdesa@gmail.com <gujardimdesa@gmail.com>
2026-08-18 09:54:57 -0300

    Feat: Criação do boiler-My-App e componentes iniciantes

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Cabecalho from './components/Cabecalho/Cabecalho';
+import Rodape from './components/Rodape/Rodape';
+
+export default function App() {
+  
+  return (
+    <div>
+      <Cabecalho/>
+      <Rodape/>
+    </div>
+  );
+}
---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,9 @@
+
+export default function Cabecalho() {
+  
+  return (
+    <div>
+        <h1>conteúdo</h1>
+    </div>
+  );
+}
