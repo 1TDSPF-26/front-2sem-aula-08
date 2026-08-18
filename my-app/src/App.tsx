d81999fd07ae11feb24026534174faf9973b92f5
enzoestanguera <enzo.estanguera@gmail.com>
2026-08-18 09:54:47 -0300

    feat: craição boiler-plate

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+export default function App(){
+
+
+  return{
+    <div>
+    <header>
+        <h1>Meu Componente</h1>
+
+        <nav>
+        <ul>
+          <li>Item-1</li>
+          <li>Item-2</li>
+          <li>Item-3</li>
+        </ul>
+        </nav>
+    </header>
+    </div>
+  }
+}
---
+++ my-app/src/assets/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,18 @@
+export default function Cabecalho(){
+
+
+
+    return(
+    <div>
+        <header><h1>Meu Componente</h1></header>
+
+        <nav>
+            <ul>
+            <li>Item-1</li>
+            <li>Item-2</li>
+            <li>Item-3</li>
+            </ul>
+        </nav>
+    </div>
+    )
+}
