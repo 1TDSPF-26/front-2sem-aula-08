fc7b20e25b58a5327027ab694742db8be4a10060
lkoprowskif <larikoprowski@gmail.com>
2026-08-18 09:53:19 -0300

    feat: my-app criado.

---
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -0,0 +1,8 @@
+export default function Conteudo(){
+
+    return(
+        <body>
+            <h1>Lari!!</h1>
+        </body>
+    )
+}
---
+++ my-app/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,8 @@
+export default function Rodape(){
+
+    return(
+        <footer>
+            <h1>fim!!</h1>
+        </footer>
+    )
+}
