8a42742d4cedfe55256a84b8f080c3327156453e
arthurarrudaa <arthur.arrruda.dev@gmail.com>
2026-08-18 09:54:24 -0300

    feat: Criação do boiler-plate e componentes iniciais

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import Body from "./components/Body/Body";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
+export default function App(){
+  return(
+    <div>
+      <Cabecalho/>
+      <Body/>
+    </div>
+  );
+}
---
+++ my-app/src/components/Body/Body.tsx
@@ -0,0 +1,8 @@
+
+export default function Body (){
+    return(
+        <div>
+            <h2>Corpo do projeto</h2>
+        </div>
+    );
+}
