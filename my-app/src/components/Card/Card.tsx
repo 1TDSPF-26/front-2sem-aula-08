4dbd8c81ed162ca2133abddf135342ec7778faf2
igorblacconaro <igorbs2910@gmail.com>
2026-08-19 12:19:56 -0300

    feat: Iniciando com props.

---
+++ my-app/src/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card(props){
+    return(
+        <div>
+            <h2>{props.tittle}</h2>
+
+        </div>
+    );
+}
--- my-app/src/components/Corpo/Corpo.tsx
+++ my-app/src/components/Corpo/Corpo.tsx
@@ -1,8 +1,11 @@
+import Card from "../Card/Card";
+
 export default function Corpo(){
     return(
         <main>
             <h2>Corpo do texto</h2>
             <p>Inserir conteúdo aqui</p>
+            <Card tittle="Meu primeiro Card"/>
         </main>
     );
 }
