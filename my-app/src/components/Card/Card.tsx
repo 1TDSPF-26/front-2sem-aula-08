294de5d6dff113eb4a62bb646876c8ce73f0332f
BryanC0staDev <bryancosta713@gmail.com>
2026-08-19 12:16:13 -0300

    feat:Limpeza do boiler plate, regularizaçã de pagina com boilers simples.

---
+++ my-app/src/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card(props) {
+    return (
+
+        <h2>{props.title}</h2>
+
+    )
+
+}
--- my-app/src/components/Main/main.tsx
+++ my-app/src/components/Main/main.tsx
@@ -1,3 +1,5 @@
+import Card from "../Card/Card";
+
 export default function Corpo(){
 
     return(
@@ -6,6 +8,8 @@ export default function Corpo(){
         <h2>Escreva seu Texto aqui!</h2>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam sint id, nam libero ex quibusdam eveniet minima officiis maiores, repudiandae voluptatibus nesciunt similique est nobis eaque voluptatum quos eum modi!</p>
+
+            <Card title = "Meu Card" />
     </main>
 
     )
