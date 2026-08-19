558533191cf58bc3cef81c8e976a62371bbce303
MatheusRodriguesSerrao <rmatheusrodrigues193@gmail.com>
2026-08-19 12:15:46 -0300

    feat : limpeza do boiler plate, miodularizacao com passagem de props simples.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,6 +1,7 @@
-import Footer from "./components/footer/footer";
-import Header from "./components/header/header";
-import Main from "./components/main/main";
+import Card from "./components/Card/Card";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 export default function App() {
   
@@ -9,6 +10,7 @@ export default function App() {
       <Header/>
       <Main/>
       <Footer/>
+      <Card/>
     </div>
   );
 }
--- my-app/src/components/main/main.tsx
+++ my-app/src/components/main/main.tsx
@@ -1,27 +1,27 @@
+import Card from "../Card/Card";
+
 export default function Main() {
     return (
-        <div>
-            <main id="inicio" className="main">
-                <div className="container">
-                <section className="hero">
-                    <h2>Meu componente</h2>
-                    <p>
-                    Este é o conteúdo principal do meu site. Uma estrutura simples,
-                    organizada e responsiva.
-                    </p>
+        <main>
+            
+            <section>
+                <h2>Meu componente</h2>
+                <p>
+                Este é o conteúdo principal do meu site. Uma estrutura simples,
+                organizada e responsiva.
+                </p>
+                <button>Saiba mais</button>
+            </section>
+            <section>
+                <h2>Sobre</h2>
+                <p>
+                lorem200 
+                </p>
+            </section>
 
-                    <button>Saiba mais</button>
-                </section>
-
-                <section id="sobre" className="content">
-                    <h2>Sobre</h2>
-                    <p>
-                    Aqui você pode colocar as informações do seu projeto,
-                    apresentação, serviços ou qualquer outro conteúdo.
-                    </p>
-                </section>
-                </div>
-            </main>
-        </div>
+            <Card title="Meu primeiro Card" nomeProduto="TV"/>
+            <Card title="Meu segundo Card" nomeProduto="Microfone"/>
+            
+        </main>
     );
 }
