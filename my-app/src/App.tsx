03c33cdd9932df9cc67223adac2b9c64412a2d88
LuizVMaciel <luizvmaciel26@gmail.com>
2026-08-19 12:20:18 -0300

    feat: limpeza do boilerplate, modularização com passagem de props simples.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Conteudo from "./components/Conteudo/Conteudo";
+import Rodape from "./components/Rodape/Rodape";
 
 export default function app(){
   return(
     <div>
       <Cabecalho/>
       <Conteudo/>
+      <Rodape/>
     </div>
   )
 }
--- my-app/src/components/Conteudo/Conteudo.tsx
+++ my-app/src/components/Conteudo/Conteudo.tsx
@@ -1,10 +1,11 @@
+import Card from "../Card/Card";
+
 export default function Conteudo(){
     return(
-        <div>
             <main>
                 <h2>Conteudo</h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas voluptates exercitationem mollitia sit quos culpa blanditiis voluptatibus iste consequatur. Sint sunt nihil, laudantium quae totam eum commodi eius ipsa optio?</p>
+                <Card title="Meu card" nomeProduto="tv"/>
             </main>
-        </div>
     );
 }
