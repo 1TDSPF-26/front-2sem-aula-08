7091947afb0f29c492ae26aaafba59d4f32bc12e
LuizVMaciel <luizvmaciel26@gmail.com>
2026-08-26 12:13:24 -0300

    feat: configuração das rotas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,12 @@
 import Cabecalho from "./components/Cabecalho/Cabecalho";
-import Conteudo from "./components/Conteudo/Conteudo";
+import { Outlet } from "react-router";
 import Rodape from "./components/Rodape/Rodape";
 
 export default function app(){
   return(
     <div>
       <Cabecalho/>
-      <Conteudo/>
+      <Outlet/>
       <Rodape/>
     </div>
   )
